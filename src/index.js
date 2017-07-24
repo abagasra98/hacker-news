cf133c23c6707652ff5c898062bb38929e25d891
Abdul Bagasra <abagas3@illinois.edu>
2017-07-24 12:15:56 -0500

    Add dependencies & set-up code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ApolloProvider, createNetworkInterface, ApolloClient} from 'react-apollo';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj5idu3amaq6y0122oqdld4l2',
+});
+
+const client = new ApolloClient({
+  networkInterface
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root'));
 registerServiceWorker();
