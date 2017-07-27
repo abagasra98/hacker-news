b919f8f73d995c10be8d9bd79105bb9da9927978
Abdul Bagasra <abagas3@illinois.edu>
2017-07-27 11:14:15 -0500

    Dynamic changing of singular vs. plurals in labels

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -6,6 +6,8 @@ import {timeDifferenceForDate} from '../utils';
 class Link extends React.Component {
   render() {
     const userId = localStorage.getItem(GC_USER_ID);
+    const voteLabel = (this.props.link.votes.length === 1) ? ' vote' : ' votes';
+    
     return (
       <div className="flex mt2 items-start">
         <div className="flex items-start">
@@ -14,7 +16,7 @@ class Link extends React.Component {
         </div>
         <div className="ml1">
           <div>{this.props.link.description} ({this.props.link.url})</div>
-          <div className="f6 lh-copy gray">{this.props.link.votes.length} votes | by {this.props.link.postedBy ? this.props.link.postedBy.name : 'Unknown'} {timeDifferenceForDate(this.props.link.createdAt)}</div>
+          <div className="f6 lh-copy gray">{this.props.link.votes.length + voteLabel} | by {this.props.link.postedBy ? this.props.link.postedBy.name : 'Unknown'} {timeDifferenceForDate(this.props.link.createdAt)}</div>
         </div>
       </div>
     );
